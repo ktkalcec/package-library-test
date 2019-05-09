c648f757698599753f1874c2834bcddd12655b15
KristijanTkalčec <kristijan.tkalcec@gmail.com>
2019-05-09 09:26:31 +0200

    Add api platform

---
+++ config/bundles.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+];
